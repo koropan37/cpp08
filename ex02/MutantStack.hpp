c0ca8be62863b97464917acdcc636987111c987b
koro <shunta37k@gmail.com>
2025-12-14 23:40:21 +0900

    12/14

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -6,6 +6,34 @@
 template<typename T>
 class MutantStack : public std::stack<T> {
  public:
+    typedef typename std::stack<T>::container_type::iterator iterator;
+    typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+
+    MutantStack() : std::stack<T>() {}
+    MutantStack(const MutantStack& other) : std::stack<T>(other) {}
+    MutantStack& operator=(const MutantStack& other) {
+        if (this != &other) {
+            std::stack<T>::operator=(other);
+        }
+        return *this;
+    }
+    ~MutantStack() {}
+
+    iterator begin() {
+        return this->c.begin();
+    }
+
+    iterator end() {
+        return this->c.end();
+    }
+
+    const_iterator begin() const {
+        return this->c.begin();
+    }
+
+    const_iterator end() const {
+        return this->c.end();
+    }
 };
 
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,28 +1,105 @@
 #include "MutantStack.hpp"
 #include <iostream>
+#include <list>
+
+#define BLACK   "\033[0;30m"
+#define RED     "\033[0;31m"
+#define GREEN   "\033[0;32m"
+#define YELLOW  "\033[0;33m"
+#define BLUE    "\033[0;34m"
+#define MAGENTA "\033[0;35m"
+#define CYAN    "\033[0;36m"
+#define WHITE   "\033[0;37m"
+#define RESET   "\033[0m"
+#define B_RED	"\033[1;31m"
+#define B_GREEN "\033[1;32m"
+#define B_CYAN  "\033[1;36m"
+
+void printColor(std::string const& str, std::string const& color) {
+    std::cout
+	<< RESET << "======="
+	<< color << str
+	<< RESET << "=======" << color << std::endl;
+}
 
 int main()
 {
-    MutantStack<int> mstack;
-    mstack.push(5);
-    mstack.push(17);
-    std::cout << mstack.top() << std::endl;
-    mstack.pop();
-    std::cout << mstack.size() << std::endl;
-    mstack.push(3);
-    mstack.push(5);
-    mstack.push(737);
-    //[...]
-    mstack.push(0);
-    MutantStack<int>::iterator it = mstack.begin();
-    MutantStack<int>::iterator ite = mstack.end();
-    ++it;
-    --it;
-    while (it != ite)
+    printColor("Subject", CYAN);
     {
-        std::cout << *it << std::endl;
+        MutantStack<int> mstack;
+        mstack.push(5);
+        mstack.push(17);
+        std::cout << mstack.top() << std::endl;
+        mstack.pop();
+        std::cout << mstack.size() << std::endl;
+        mstack.push(3);
+        mstack.push(5);
+        mstack.push(737);
+        //[...]
+        mstack.push(0);
+        MutantStack<int>::iterator it = mstack.begin();
+        MutantStack<int>::iterator ite = mstack.end();
         ++it;
+        --it;
+        while (it != ite)
+        {
+            std::cout << *it << std::endl;
+            ++it;
+        }
+        std::stack<int> s(mstack);
     }
-    std::stack<int> s(mstack);
+    printColor("List", CYAN);
+    {
+        std::list<int> list;
+        list.push_back(5);
+        list.push_back(17);
+        std::cout << list.back() << std::endl;
+        list.pop_back();
+        std::cout << list.size() << std::endl;
+        list.push_back(3);
+        list.push_back(5);
+        list.push_back(737);
+        //[...]
+        list.push_back(0);
+        std::list<int>::iterator it = list.begin();
+        std::list<int>::iterator ite = list.end();
+        ++it;
+        --it;
+        while (it != ite)
+        {
+            std::cout << *it << std::endl;
+            ++it;
+        }
+        std::list<int> s(list);
+    }
+    printColor("Const", YELLOW);
+    {
+        MutantStack<int> mstack;
+        mstack.push(5);
+        mstack.push(17);
+        std::cout << mstack.top() << std::endl;
+        mstack.pop();
+        std::cout << mstack.size() << std::endl;
+        mstack.push(3);
+        mstack.push(5);
+        mstack.push(737);
+        mstack.push(0);
+        MutantStack<int>::const_iterator it = mstack.begin();
+        MutantStack<int>::const_iterator ite = mstack.end();
+        while (it != ite)
+        {
+            std::cout << *it << std::endl;
+            ++it;
+        }
+        mstack.pop();
+        std::cout << std::endl;
+
+        std::stack<int> s(mstack);
+        std::cout << mstack.top() << std::endl;
+        std::cout << s.top() << std::endl;
+        mstack.pop();   
+        std::cout << mstack.top() << std::endl;
+        std::cout << s.top() << std::endl; 
+    }
     return 0;
 }
